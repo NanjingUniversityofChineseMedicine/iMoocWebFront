bf98ae364916c5c6d0b7aee89bbd6ebb71c3d28c
Zhengdong Pan <donggnij@foxmail.com>
2018-04-07 16:05:42 +0800

    broadcast inneed

--- static/js/Broadcast.js
+++ static/js/Broadcast.js
@@ -1,17 +1,35 @@
-$(function(){
-  var $refreshButton = $('#refresh');
-  var $results = $('#css_result');
+var player = videojs('video');
+player.playlist([{
+  sources: [{
+    src: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/sintel/poster.png'
+}, {
+  sources: [{
+    src: 'http://media.w3.org/2010/05/bunny/trailer.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+}, {
+  sources: [{
+    src: 'http://vjs.zencdn.net/v/oceans.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://www.videojs.com/img/poster.jpg'
+}, {
+  sources: [{
+    src: 'http://media.w3.org/2010/05/bunny/movie.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/bunny/poster.png'
+}, {
+  sources: [{
+    src: 'http://media.w3.org/2010/05/video/movie_300.mp4',
+    type: 'video/mp4'
+  }],
+  poster: 'http://media.w3.org/2010/05/video/poster.png'
+}]);
 
-  function refresh(){
-    var css = $('style.cp-pen-styles').text();
-    $results.html(css);
-  }
-
-  refresh();
-  $refreshButton.click(refresh);
-
-  // Select all the contents when clicked
-  $results.click(function(){
-    $(this).select();
-  });
-});
+// Play through the playlist automatically.
+player.playlist.autoadvance(0);
